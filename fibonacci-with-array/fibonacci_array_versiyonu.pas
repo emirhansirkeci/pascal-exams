39194b79e58eb814636177cb93db45b4093af741
unknown <newgds.27@gmail.com>
2022-12-03 00:38:01 +0300

    Delete wrong examples

---
+++ fibonacci-with-array/fibonacci_array_versiyonu.pas
@@ -0,0 +1,39 @@
+uses crt;
+var
+  adet:integer;
+  sayilar:array[0..999] of integer;
+
+label tekrarSor;
+
+procedure fibonacci(adet:integer);
+var i:integer;
+
+begin
+  sayilar[1] := 0;
+  sayilar[0] := 1;
+
+  for i := 1 to adet do begin
+    sayilar[i+1] := sayilar[i] + sayilar[i-1];
+
+    write(sayilar[i], ' ');
+  end;
+
+end;
+
+begin
+  tekrarSor:
+  write('Kac adet fibonacci sayisi gormek istiyorsunuz: ');
+  readln(adet);
+
+  if adet <= 0 then begin
+    writeln('Gecersiz sayi girdiniz devam etmek icin bir tusa basin');
+    readln;
+    clrscr;
+    goto tekrarSor;
+  end;
+
+  fibonacci(adet);
+
+  readln;
+end.
+
